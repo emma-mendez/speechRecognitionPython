7226d8cd40b841e0a84f3f60a6868115263c8fa0
Emma Mendez <emmamendez07@gmail.com>
2020-12-15 16:22:22 +0000

    Speech to text officially working

--- app.py
+++ app.py
@@ -13,10 +13,9 @@ def index():
         if "file" not in request.files:
             return redirect(request.url)
 
-        file = request.files.["file"]
+        file = request.files["file"]
         if file.filename == "":
-            return  redirect(request.url)
-
+            return redirect(request.url)
 
         if file:
             recognizer = sr.Recognizer()
